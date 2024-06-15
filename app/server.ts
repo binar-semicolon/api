a6bba735c76e4dc04623f5eb0143a888c8ec3c4d
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-06-15 13:06:02 +0700

    feat: add cors and body parsing

--- app/server.ts
+++ app/server.ts
@@ -1,9 +1,15 @@
 import { contract } from "./contract";
 import { user } from "./routes/user";
 import { createExpressEndpoints } from "@ts-rest/express";
+import bodyParser from "body-parser";
+import cors from "cors";
 import express from "express";
 
 const app = express();
+
+app.use(cors()); // eslint-disable-line @typescript-eslint/no-unsafe-call
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 createExpressEndpoints(
   contract,
