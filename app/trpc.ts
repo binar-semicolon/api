e23063d2dbcb8bba1252f9ceef41d0488668e498
patrickariel <161032380+patrickariel@users.noreply.github.com>
2024-06-16 21:24:16 +0700

    feat: add superjson transformer

--- app/trpc.ts
+++ app/trpc.ts
@@ -1,12 +1,13 @@
 import { initTRPC } from "@trpc/server";
 import trpcExpress from "@trpc/server/adapters/express";
+import superjson from "superjson";
 
 export const createContext = ({
   req, // eslint-disable-line @typescript-eslint/no-unused-vars
   res, // eslint-disable-line @typescript-eslint/no-unused-vars
 }: trpcExpress.CreateExpressContextOptions) => ({}); // no context
 
-const t = initTRPC.context<Context>().create();
+const t = initTRPC.context<Context>().create({ transformer: superjson });
 
 type Context = Awaited<ReturnType<typeof createContext>>;
 
